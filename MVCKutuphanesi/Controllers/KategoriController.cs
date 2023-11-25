8ba6daaec010ccd196ae6d3cf304a82914790cb5
Kübra Bol <kubra1272@icloud.com>
2023-11-25 15:17:29 +0300

    CategoryUpdate

--- MVCKutuphanesi/Controllers/KategoriController.cs
+++ MVCKutuphanesi/Controllers/KategoriController.cs
@@ -3,15 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCKutuphanesi.Models.Entity;
 
 namespace MVCKutuphanesi.Controllers
 {
     public class KategoriController : Controller
     {
         // GET: Kategori
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities(); // içerisinde tabloları tutacak
         public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORI.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult KategoriEkle()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult KategoriEkle(TBLKATEGORI p)
+        {
+            db.TBLKATEGORI.Add(p);
+            db.SaveChanges(); // değişiklikleri kaydet
+            return View();
+        }
+        public ActionResult KategoriSil(int id)
+        {
+            var kategori = db.TBLKATEGORI.Find(id);
+            db.TBLKATEGORI.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+       
+
+
     }
 }
