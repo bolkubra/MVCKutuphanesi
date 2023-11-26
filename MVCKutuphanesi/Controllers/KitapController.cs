26a2fa6ef8fd9d68bd0f1aa3af89607e81398d31
Kübra Bol <kubra1272@icloud.com>
2023-11-26 18:32:09 +0300

    kitapUpdate

--- MVCKutuphanesi/Controllers/KitapController.cs
+++ MVCKutuphanesi/Controllers/KitapController.cs
@@ -1,17 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCKutuphanesi.Models.Entity;
 
 namespace MVCKutuphanesi.Controllers
 {
     public class KitapController : Controller
     {
         // GET: Kitap
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
         public ActionResult Index()
         {
+            var kitaplar = db.TBLKITAP.ToList();
+            return View(kitaplar);
+        }
+        [HttpGet]
+        public ActionResult KitapEkle() // sayfa yüklendğinde içinde veriler olması için
+        {
+            List<SelectListItem> deger1 = (from i in db.TBLKATEGORI.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = i.AD,
+                                               Value = i.ID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1; //deger1 den değeri taşı
+
+            List<SelectListItem> deger2 = (from i in db.TBLYAZAR.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = i.AD +' '+ i.SOYAD ,
+                                               Value = i.ID.ToString()
+                                           }).ToList();
+            ViewBag.dgr2 = deger2; 
             return View();
+        }
+        [HttpPost]
+        public ActionResult KitapEkle(TBLKITAP p) 
+        {
+            var ktg = db.TBLKATEGORI.Where(k => k.ID == p.TBLKATEGORI.ID).FirstOrDefault();
+            var yzr = db.TBLYAZAR.Where(y => y.ID == p.TBLYAZAR.ID).FirstOrDefault();
+            p.TBLKATEGORI=ktg;
+            p.TBLYAZAR=yzr;
+            db.TBLKITAP.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+           
+        }
+
+        public ActionResult KitapSil(int id)
+        {
+            var kitap = db.TBLKITAP.Find(id);
+            db.TBLKITAP.Remove(kitap);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
