7f7cba24c0f4f3c5f0b7af902d0ce0e19ae3bf60
Kübra Bol <kubra1272@icloud.com>
2023-11-27 17:59:23 +0300

    PersonelAdded

---
+++ MVCKutuphanesi/Controllers/PersonelController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCKutuphanesi.Models.Entity;
+
+namespace MVCKutuphanesi.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLPERSONEL.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult PersonelEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PersonelEkle(TBLPERSONEL p)
+        {
+            if (!ModelState.IsValid) // işlem sağlanmadıysa
+            {
+                return View("PersonelEkle");
+            }
+            db.TBLPERSONEL.Add(p);
+            db.SaveChanges(); 
+            return View();
+        }
+        public ActionResult PersonelSil(int id)
+        {
+            var personel = db.TBLPERSONEL.Find(id);
+            db.TBLPERSONEL.Remove(personel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult PersonelGetir(int id)
+        {
+            var prs = db.TBLPERSONEL.Find(id);
+            return View("PersonelGetir", prs);
+        }
+        public ActionResult PersonelGuncelle(TBLPERSONEL p)
+        {
+            var prs = db.TBLPERSONEL.Find(p.ID);
+            prs.PERSONEL = p.PERSONEL;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
