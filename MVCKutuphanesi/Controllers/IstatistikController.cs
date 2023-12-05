aa223598c66832bf1485d0b912a7d2fe6ee7fa4f
Kübra Bol <kubra1272@icloud.com>
2023-12-05 18:54:22 +0300

    cezaUpdate

--- MVCKutuphanesi/Controllers/IstatistikController.cs
+++ MVCKutuphanesi/Controllers/IstatistikController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -17,5 +18,24 @@ namespace MVCKutuphanesi.Controllers
         {
             return View();
         }
+        public ActionResult HavaKart()
+        {
+            return View();
+        }
+        public ActionResult Galeri()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult resimyukle (HttpPostedFileBase dosya)
+        {
+            if (dosya.ContentLength > 0)
+            {
+                string dosyayolu = Path.Combine(Server.MapPath("~/web2/resimler/"), Path.GetFileName(dosya.FileName));
+                dosya.SaveAs(dosyayolu);
+            }
+            return RedirectToAction("Galeri");
+        }
+
     }
 }
