1a9ff10f4456138c07109c85660427f906bff52a
Kübra Bol <kubra1272@icloud.com>
2023-12-06 21:12:31 +0300

    LinqKullanımı

--- MVCKutuphanesi/Controllers/IstatistikController.cs
+++ MVCKutuphanesi/Controllers/IstatistikController.cs
@@ -4,14 +4,23 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCKutuphanesi.Models.Entity;
 
 namespace MVCKutuphanesi.Controllers
 {
     public class IstatistikController : Controller
     {
         // GET: Istatistik
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
         public ActionResult Index()
         {
+            var deger1 = db.TBLUYE.Count();
+            var deger2 = db.TBLKITAP.Count();
+            var deger3 = db.TBLCEZALAR.Sum(x => x.PARA);
+            
+            ViewBag.dgr1 = deger1;
+            ViewBag.dgr2 = deger2;
+            ViewBag.dgr3 = deger3;
             return View();
         }
         public ActionResult Hava()
@@ -35,6 +44,10 @@ namespace MVCKutuphanesi.Controllers
                 dosya.SaveAs(dosyayolu);
             }
             return RedirectToAction("Galeri");
+        }
+        public ActionResult LinqKart()
+        {
+            return View();
         }
 
     }
