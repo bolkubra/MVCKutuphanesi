17786a39ce2d89ebf02762863ac85fc97b3b9674
Kübra Bol <kubra1272@icloud.com>
2023-12-09 00:54:12 +0300

    PanelAdded

--- MVCKutuphanesi/Controllers/LoginController.cs
+++ MVCKutuphanesi/Controllers/LoginController.cs
@@ -3,15 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using MVCKutuphanesi.Models.Entity;
 
 namespace MVCKutuphanesi.Controllers
 {
     public class LoginController : Controller
     {
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
         // GET: Login
+        [HttpGet]
         public ActionResult GirisYap()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult GirisYap(TBLUYE t)
+        {
+            var bilgiler = db.TBLUYE.FirstOrDefault(x=>x.EMAIL==t.EMAIL && x.SIFRE==t.SIFRE);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.EMAIL, false);
+                Session["Email"] = bilgiler.EMAIL.ToString();
+                //TempData["Ad"] = bilgiler.AD.ToString();
+                //TempData["Id"] = bilgiler.ID.ToString();
+                //TempData["SoyAd"] = bilgiler.SOYAD.ToString();
+                //TempData["KullanıcıAdı"] = bilgiler.KULLANICIADI.ToString();
+                //TempData["Sifre"] = bilgiler.SIFRE.ToString();
+                //TempData["Telefon"] = bilgiler.TELEFON.ToString();
+                //TempData["Okul"] = bilgiler.OKUL.ToString();
+                return RedirectToAction("Index", "Panelim");
+            }
+            else
+            {
+                return View();
+            }
+           
         }
     }
 }
--- MVCKutuphanesi/Controllers/PanelimController.cs
+++ MVCKutuphanesi/Controllers/PanelimController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCKutuphanesi.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,28 @@ namespace MVCKutuphanesi.Controllers
     public class PanelimController : Controller
     {
         // GET: Panelim
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        [Authorize]
+        [HttpGet]
         public ActionResult Index()
         {
+            var uyemail = (string)Session["Email"];
+            var degerler = db.TBLUYE.FirstOrDefault(z => z.EMAIL == uyemail);
+            return View(degerler);
+        }
+        [HttpPost]
+        public ActionResult Index(TBLUYE t)
+        {
+            var kullanici = (string)Session["Email"];
+            var uye = db.TBLUYE.FirstOrDefault(x => x.EMAIL == kullanici);
+            uye.SIFRE = t.SIFRE;
+            uye.AD = t.AD;
+            uye.SOYAD = t.SOYAD;
+            uye.KULLANICIADI = t.KULLANICIADI;
+            uye.FOTOGRAF = t.FOTOGRAF;
+            uye.OKUL=t.OKUL;
+            uye.TELEFON = t.TELEFON;
+            db.SaveChanges();
             return View();
         }
     }
