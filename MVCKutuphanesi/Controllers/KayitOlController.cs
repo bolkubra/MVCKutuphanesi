5a21367f95380cae9daeba962c26a4fc9a9c3910
Kübra Bol <kubra1272@icloud.com>
2023-12-08 17:15:53 +0300

    LoginRegisterAdded

---
+++ MVCKutuphanesi/Controllers/KayitOlController.cs
@@ -0,0 +1,31 @@
+﻿using MVCKutuphanesi.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCKutuphanesi.Controllers
+{
+    public class KayitOlController : Controller
+    {
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        // GET: KayitOl
+        [HttpGet]
+        public ActionResult Kayit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Kayit(TBLUYE p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Kayit");
+            }
+            db.TBLUYE.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
