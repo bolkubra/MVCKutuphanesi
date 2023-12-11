ea51c92a4795abdeb44927c8585b221c0658409d
Kübra Bol <kubra1272@icloud.com>
2023-12-11 19:41:20 +0300

    refactor

--- MVCKutuphanesi/Controllers/OduncController.cs
+++ MVCKutuphanesi/Controllers/OduncController.cs
@@ -19,14 +19,51 @@ namespace MVCKutuphanesi.Controllers
         [HttpGet]
         public ActionResult OduncVer()
         {
+            List<SelectListItem> deger1 = (from x in db.TBLUYE.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.AD +" "+ x.SOYAD,
+                                               Value = x.ID.ToString()
+                                               
+                                           }
+                                          ).ToList() ;
+
+            List<SelectListItem> deger2 = (from x in db.TBLKITAP.Where(x=>x.DURUM==true).ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.AD,
+                                               Value = x.ID.ToString()
+
+                                           }
+                                          ).ToList();
+
+            List<SelectListItem> deger3 = (from x in db.TBLPERSONEL.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.PERSONEL,
+                                               Value = x.ID.ToString()
+
+                                           }
+                                        ).ToList();
+
+            ViewBag.dgr1 = deger1;
+            ViewBag.dgr2 = deger2;
+            ViewBag.dgr3 = deger3;
             return View();
         }
         [HttpPost]
         public ActionResult OduncVer(TBLHAREKET p)
         {
+            var d1 = db.TBLUYE.Where(x => x.ID == p.TBLUYE.ID).FirstOrDefault(); // dropdowndan gelen değerleri değişkenlere atadık
+            var d2 = db.TBLKITAP.Where(y => y.ID == p.TBLKITAP.ID).FirstOrDefault();
+            var d3 = db.TBLPERSONEL.Where(z => z.ID == p.TBLPERSONEL.ID).FirstOrDefault();
+            p.TBLUYE = d1;
+            p.TBLKITAP = d2;
+            p.TBLPERSONEL = d3; 
+
             db.TBLHAREKET.Add(p);
-            db.SaveChanges(); 
-            return View();
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
         public ActionResult Odunciade(TBLHAREKET p)
         {
